3b9ba133a270e803c6c95262f174ee645800ecd3
JuanLopp18 <juank.lopezs12@gmail.com>
2024-12-06 16:32:17 +0100

    Primer commit

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
+import Main from "../components/Main";
+import "../global.css"; // Ruta correcta según tu estructura
 
 export default function _layout() {
     return(
-        <View>
-            <Text>Layout</Text>
+        <View className="flex-1 bg-black">
+           <Main/>
         </View>
     )
 }
