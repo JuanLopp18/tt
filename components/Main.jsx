525042f3d89053f68bddeddab65d9a3d7c0486ac
JuanLopp18 <juank.lopezs12@gmail.com>
2024-12-06 16:49:00 +0100

    Texto a Bold

--- components/Main.jsx
+++ components/Main.jsx
@@ -5,7 +5,7 @@ import { Text, View } from "react-native";
 export default function Main() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-white">Open up App.js to start working on your app!</Text>
+      <Text className="text-white font-bold">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
