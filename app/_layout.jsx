8a3f843c331731d06027133581592506b8654707
JuanLopp18 <juank.lopezs12@gmail.com>
2024-12-07 12:54:21 +0100

    Theme.context copy-paste

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,12 +1,16 @@
 import { View, Text } from "react-native";
 import React from "react";
 import Main from "../components/Main";
+import { ThemeProvider } from "@react-navigation/native"
 import "../global.css"; // Ruta correcta según tu estructura
+import { Stack } from "expo-router";
 
 export default function _layout() {
     return(
-        <View className="flex-1 bg-black">
-           <Main/>
-        </View>
+        <ThemeProvider>
+            <Stack screenOptions={{headerShown: false}}>
+                <Stack.Screen name="index" />
+            </Stack>
+        </ThemeProvider>
     )
 }
---
+++ context/theme.context.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function ThemeContext() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-white font-bold">Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
